fafc0a3996be7643652fe1c194af1b767c24fc46
Alessandro Balzano <alfateam123@users.noreply.github.com>
2016-10-04 09:51:58 +0200

    Fix issue #2 (#3)
    * Update imgur upload API
    
    Perform a minor fix if a proxy is not set.
    
    * Fix an issue with spaces in file path
    
    * remove useless comment

--- SauceNao-Windows/Imgur.cs
+++ SauceNao-Windows/Imgur.cs
@@ -12,15 +12,15 @@ namespace SauceNao_Windows {
   static class Imgur {
     public static string Upload(string path, string apiKey) {
       using (var w = new WebClient()) {
+        w.Headers.Add("Authorization: Client-ID " + apiKey);
         var values = new NameValueCollection {
-          { "key",   apiKey },
           { "image", Convert.ToBase64String(File.ReadAllBytes(@path)) }
         };
 
-        string response = System.Text.Encoding.UTF8.GetString(w.UploadValues("http://imgur.com/api/upload.json", values));
+        string response = System.Text.Encoding.UTF8.GetString(w.UploadValues("https://api.imgur.com/3/upload", values));
         Console.WriteLine(response);
         dynamic dynObj = JsonConvert.DeserializeObject(response);
-        return dynObj.rsp.image.original_image;
+        return dynObj.data.link;
       }
     }
   }
--- SauceNao-Windows/Program.cs
+++ SauceNao-Windows/Program.cs
@@ -13,8 +13,9 @@ namespace SauceNao_Windows {
   class Program {
     /* TODO: struct User; struct Picture; abstract class Sauce; class Sauces_GenericSauce */
 
+
     static void Main(string[] args) {
-      if(args.Length < 3) {
+      if (args.Length < 3) {
         Console.WriteLine("Usage: SauceNao-Windows.exe <SAUCENAO_API_KEY> <IMGUR_API_KEY> <IMAGE_PATH> [PROXY IP:PORT]");
         Console.ReadLine();
         return;
@@ -23,8 +24,9 @@ namespace SauceNao_Windows {
       string   SAUCENAO_API_KEY = args[0];
       string   IMGUR_API_KEY    = args[1];
       string   IMAGE_PATH       = args[2];
-      WebProxy PROXY            = args.Length >= 3 ? new WebProxy(args[3]) : null;
+      WebProxy PROXY            = args.Length >= 4 ? new WebProxy(args[3]) : null;
 
+      Console.WriteLine("The requested image is: " + IMAGE_PATH);
       if(!File.Exists(IMAGE_PATH)) {
         Console.WriteLine("Given file was not found.");
         Console.ReadLine();
